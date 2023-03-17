17c70a9fc91af9e6bfedcad2f16b2352c7eabe73
OriolGarciaFores <ori_ugf@hotmail.com>
2023-03-17 22:43:44 +0100

    Generación de launcher.

---
+++ executable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try{
+            
+        Process process = new Process();
+        process.StartInfo.FileName = @"java\bin\java.exe";
+        process.StartInfo.Arguments = "-jar spaceship.jar";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+        process.Start();
+        process.Close();
+        } catch(Exception ex) {
+            Console.WriteLine("Error al ejecutar el archivo spaceship.jar: " + ex.Message);
+        } finally {
+            Console.WriteLine("Proceso ejecución Spaceship finalizado.");
+            Environment.Exit(0);
+        }
+        
+    }
+}
